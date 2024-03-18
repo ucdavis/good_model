737b361ab3636a515e0bd518e52b7f648de77ff4
peterambiel <paambiel@ucdavis.edu>
2024-03-18 15:20:41 -0700

    Updated class mapping to reflect new data structure

--- toy_src/good_model_working/class_mapping.py
+++ toy_src/good_model_working/class_mapping.py
@@ -6,16 +6,8 @@ from .Load import Load
 
 class_dict_for_region = {
     'generators' : Generator,
-    'solar_cost': Solar,
-    'solar_gen': Solar,
-    'solar_max_capacity': Solar,
-    'solar_installed_capacity': Solar,
-    'solar_transmission_cost': Solar,
-    'wind_cost': Wind,
-    'wind_gen': Wind,
-    'wind_max_capacity': Wind,
-    'wind_installed_capacity': Wind,
-    'wind_transmission_cost': Wind,
-    'Energy Storage': Storage,
+    'solar': Solar, 
+    'wind': Wind, 
+    'storage': Storage,
     'load': Load,
 }
