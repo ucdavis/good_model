1ff2f09bf61443b959591bcbfe27117743efd295
peterambiel <paambiel@ucdavis.edu>
2024-04-01 11:57:36 -0700

    Update all modules/ classes

--- toy_src/good_model_working/Generator.py
+++ toy_src/good_model_working/Generator.py
@@ -7,111 +7,85 @@ class Generator:
         self.gen_capacity = {}
         self.gen_type = []
         self.fuel_type = []
-
+        
         for data in generators:
 
             gen_type_data = data.get('parameters',[])
 
+            total_capacity = 0
+            total_cost = 0
+            hold_capacity = []
+            hold_cost = []
+            wt_capacity = []
+
             for params in gen_type_data: 
 
-                gen_id = params.get('plant_type', 0)
-                gen_data = params.get('generation', [])
+                gen_id = params.get('gen_type', 0)
+                gen_data = params.get('values', {})
 
-                total_capacity = 0
-                total_cost = 0
-                hold_capacity = []
-                hold_cost = []
-                wt_capacity = []
+                hold_capacity.append(gen_data.get('capacity', 0))
+                total_capacity += gen_data.get('capacity',0)
+                hold_cost.append(gen_data.get('cost', 0))
 
-                for gen_params in gen_data: 
-
-                    fuel_type = gen_params.get('fuel_type', 0)
-                    self.fuel_type.append(fuel_type)
-                    values = gen_params.get('values', {})
-
-                    gen_key = (gen_id, fuel_type)
-                    if gen_key not in self.gen_capacity.keys():
-                        self.gen_capacity[gen_key]= (values.get('capacity', 0))
-                    else: 
-                        self.gen_capacity[gen_key] += (values.get('capacity', 0))
+                if gen_id not in self.gen_capacity.keys():
+                    self.gen_capacity[gen_id]= (gen_data.get('capacity', 0))
+                else: 
+                    self.gen_capacity[gen_id] += (gen_data.get('capacity', 0))
                     
-                    hold_capacity.append(values.get('capacity', 0))
-                    total_capacity += values.get('capacity',0)
-                    hold_cost.append(values.get('cost', 0))
-
-                for (gen, fuel), value in self.gen_capacity.items(): 
+                for gen_id, value in self.gen_capacity.items(): 
                     wt_capacity.append(value/ total_capacity)
 
                 for i in range(len(hold_capacity)):
                     wt_cost = wt_capacity[i] * hold_cost[i]
                     total_cost += wt_cost
-                    fuel_key = (gen_id, self.fuel_type[i])
-                    if not fuel_key in self.gen_cost.keys():
-                        self.gen_cost[fuel_key] = total_cost
+                    if not gen_id in self.gen_cost.keys():
+                        self.gen_cost[gen_id] = total_cost
                     else: 
                         continue
 
     def parameters(self, model):
 
-        # cost_keys = [key for key in self.gen_cost.keys()]
-        # cap_keys = [key for key in self.gen_capacity.keys()]
+        model.add_component(
+            self.region_id + '_gencost', 
+            pyomo.Param(model.gen, initialize=self.gen_cost, within=pyomo.Reals)
+        )
 
-        # if cost_keys not in model.g: 
-
-        gencost = pyomo.Param(model.g_gf, initialize=self.gen_cost, within=pyomo.Reals)
-        setattr(model, self.region_id + '_gencost', gencost)
-
-        # if cap_keys not in model.g:
-
-        genMax = pyomo.Param(model.g_gf, initialize=self.gen_capacity, within=pyomo.Reals)
-        setattr(model, self.region_id + '_genMax', genMax)
-
-        return model
-
+        model.add_component(
+            self.region_id + '_genMax', 
+            pyomo.Param(model.gen, initialize=self.gen_capacity, within=pyomo.Reals)
+        )
 
     def variables(self, model):
 
-        if hasattr(model, self.region_id + '_gencost'):
-        
-            self.generation = pyomo.Var(model.g_gf, model.t, within=pyomo.NonNegativeReals)
-            setattr(model, self.region_id + '_generation', self.generation)
-
-            print('gen_vars')
-            print(self.region_id)
-
-        return model
+        model.add_component(
+            self.region_id + '_generation',
+            pyomo.Var(model.gen, model.t, domain=pyomo.NonNegativeReals)
+        )
 
     def objective(self, model):
 
         gen_cost_term = 0
 
-        if hasattr(model, self.region_id + '_generation'):
+        gen_indices = [g for g in model.gen if g in getattr(model, self.region_id + '_gencost')]
 
-            gen_cost_term = pyomo.quicksum(
-                getattr(model, self.region_id + '_generation')[g_gf,t] * getattr(model, self.region_id + '_gencost')[g_gf]
-                for g_gf in model.g_gf
-                for t in model.t)
+        gen_cost_term = pyomo.quicksum(
+            getattr(model, self.region_id + '_generation')[g,t] * getattr(model, self.region_id + '_gencost')[g]
+            for g in gen_indices
+            for t in model.t)
             
         return gen_cost_term
 
     def constraints(self, model):
-
-        if hasattr(model, self.region_id + '_generation'): 
             
-            generator_constraints = {}
-
-            for g_gf in model.g_gf:
-                for t in model.t:  
-                    gen_limits_rule = pyomo.ConstraintList()
-                    constraint_expr = (
-                        getattr(model, self.region_id + '_genMax')[g_gf] - getattr(model, self.region_id + '_generation')[g_gf, t]
-                        >= 0 
-                    )  
-
-                    gen_limits_rule.add(constraint_expr)                   
-                    generator_constraints.setdefault(g, {}).setdefault(gf, {})[t] = gen_limits_rule
-                        
-            setattr(model, self.region_id + '_gen_limits_rule', generator_constraints)
-
-        return model
+        def generator_constraints(model, g, t): 
+            if g in getattr(model, self.region_id + '_genMax'): 
+                return getattr(model, self.region_id + '_genMax')[g] - getattr(model, self.region_id + '_generation')[g, t] >= 0 
+            else: 
+                return pyomo.Constraint.Skip
+        
+        model.add_component(
+            self.region_id + '_gen_limits_rule', 
+            pyomo.Constraint(model.gen, model.t, rule=generator_constraints)
+        )
+    
 
--- toy_src/good_model_working/Storage.py
+++ toy_src/good_model_working/Storage.py
@@ -14,93 +14,100 @@ class Storage:
             self.cost = data.get('cost', 0)
             self.storage_flow_limit = storage_flow_limit
 
+
     def parameters(self, model):
 
-        storCap = pyomo.Param(initialize=self.storage_capacity)
-        setattr(model, self.region_id + '_storCap', storCap)
+        model.add_component(
+            self.region_id + '_storCap',
+            pyomo.Param(initialize=self.storage_capacity)
+        )
 
-        storEff = pyomo.Param(initialize=self.efficiency)
-        setattr(model, self.region_id + '_storEff', storEff)
-
-        storCost = pyomo.Param(self.region_id, initialize=self.cost)
-        setattr(model, self.region_id + '_storCost', storCost)
-
-        storFlowCap = pyomo.Param(initialize=self.storage_flow_limit)
-        setattr(model, self.region_id + '_storFlowCap', storFlowCap)
+        model.add_component(
+            self.region_id + '_storEff',
+            pyomo.Param(initialize=self.efficiency)
+        )
+     
+        model.add_component(
+            self.region_id + '_storCost',
+            pyomo.Param(self.region_id, initialize=self.cost)
+        )
+ 
+        model.add_component(
+            self.region_id + '_storFlowCap',
+            pyomo.Param(initialize=self.storage_flow_limit)
+        )
 
 
     def variables(self, model):
 
-        storSOC = pyomo.Var(model.t, within=pyomo.NonNegativeReals)
-        setattr(model, self.region_id + '_storSOC', storSOC)
+        model.add_component(
+            self.region_id + '_storSOC',
+            pyomo.Var(model.t, within=pyomo.NonNegativeReals)
+        )
 
-        storCharge = pyomo.Var(model.t, within=pyomo.NonNegativeReals)
-        setattr(model, self.region_id + '_storCharge', storCharge)
+        model.add_component(
+            self.region_id + '_storCharge',
+            pyomo.Var(model.t, within=pyomo.NonNegativeReals)
+        )
+        
+        model.add_component(
+            self.region_id + '_storDischarge', 
+            pyomo.Var(model.t, within=pyomo.NonNegativeReals)
+        )
 
-        storDischarge = pyomo.Var(model.t, within=pyomo.NonNegativeReals)
-        setattr(model, self.region_id + '_storDischarge', storDischarge)
-
-        print('storage_vars')
-
-        return model
 
     def objective(self, model): 
 
         return 0 
    
+
     def constraints(self, model):
 
-        maxStorage_rule = {}
-        storSOC_rule = {}
-        storCharge_rule = {}
-        storDischarge_rule = {}
+        def maxStorage_rule(model, t): 
+            return getattr(model, self.region_id + '_storCap') - getattr(model, self.region_id + '_storSOC')[t] >= 0
 
-        for t in model.t:
-            # Storage capacity constraint
-            constraint_expr = (
-                getattr(model, self.region_id + '_storCap') - getattr(model, self.region_id + '_storSOC')[t] 
-                >= 0
-            )
-            maxStorage_rule[t] = pyomo.Constraint(expr=constraint_expr)
-
-            # Storage state-of-charge constraint
+        model.add_component(
+            self.region_id + '_storage_capacity_rule', 
+            pyomo.Constraint(model.t, rule=maxStorage_rule)
+        )
+        
+        def storSOC_rule(model, t): 
             if t == min(model.t):
-                constraint_expr = (
-                    getattr(model, self.region_id + '_storSOC')[t] 
-                    == 0
-                )
-
-                storSOC_rule[t] = pyomo.Constraint(expr=constraint_expr)
+               return getattr(model, self.region_id + '_storSOC')[t] == 0
+        
             else:
                 t_1 = t - 1
-                constraint_expr = (getattr(model, self.region_id + '_storSOC')[t] - getattr(model, self.region_id + '_storSOC')[t_1]
-                    - getattr(model, self.region_id + '_storCharge')[t_1] * getattr(model, self.region_id + '_storEff')
+                return (getattr(model, self.region_id + '_storSOC')[t] - getattr(model, self.region_id + '_storSOC')[t_1] - getattr(model, self.region_id + '_storCharge')[t_1] * getattr(model, self.region_id + '_storEff')
                     + getattr(model, self.region_id + '_storDischarge')[t_1]
                     == 0
-                )
-                
-                storSOC_rule[t] = pyomo.Constraint(expr=constraint_expr)
+                )   
 
-            # Storage flow-in (charge) constraint
-            constraint_expr = (getattr(model, self.region_id + '_storCap') * getattr(model, self.region_id + '_storFlowCap')
+        model.add_component(
+            self.region_id + '_storage_SOC_rule',
+            pyomo.Constraint(model.t, rule=storSOC_rule)
+        )
+        
+
+        def storCharge_rule(model, t): 
+            return (getattr(model, self.region_id + '_storCap') * getattr(model, self.region_id + '_storFlowCap')
                 - getattr(model, self.region_id + '_storCharge')[t]
                 >= 0
             )
 
-            storCharge_rule[t] = pyomo.Constraint(expr=constraint_expr)
-                
-            # Storage flow-out (discharge) constraints
-            constraint_expr = (getattr(model, self.region_id + '_storCap') * getattr(model, self.region_id + '_storFlowCap')
+        model.add_component(
+            self.region_id + '_stor_charge_rule', 
+            pyomo.Constraint(model.t, rule=storCharge_rule)
+        )
+
+        def storDischarge_rule(model, t): 
+            return (getattr(model, self.region_id + '_storCap') * getattr(model, self.region_id + '_storFlowCap')
                 - getattr(model, self.region_id + '_storDischarge')[t]
                 >= 0
             )
 
-            storDischarge_rule[t] = pyomo.Constraint(expr=constraint_expr)
-
-        setattr(model, self.region_id + '_storage_capacity_rule', maxStorage_rule)
-        setattr(model, self.region_id + '_storage_SOC_rule', storSOC_rule)
-        setattr(model, self.region_id + '_stor_discharge_rule', storDischarge_rule)
-        setattr(model, self.region_id + '_stor_charge_rule', storCharge_rule)
-
-        return model
+        model.add_component(
+            self.region_id + '_stor_discharge_rule',
+            pyomo.Constraint(model.t, rule=storDischarge_rule)
+        )
+        
 
